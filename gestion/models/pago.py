ec68f06da528aee52ce9989a4ac9df20a9161396
Diego-Dlh <diegodelahozb@hotmail.com>
2025-08-07 12:08:24 -0500

    Update fecha field in Pago model to use default timezone now

--- gestion/models/pago.py
+++ gestion/models/pago.py
@@ -1,10 +1,12 @@
 from django.db import models
 from .prestamo import Prestamo
+from django.utils import timezone
 
 class Pago(models.Model):
     prestamo = models.ForeignKey(Prestamo, on_delete=models.CASCADE, related_name="pagos")
     monto_pagado = models.IntegerField()
-    fecha = models.DateTimeField(auto_now_add=True)
+    fecha = models.DateTimeField(default=timezone.now, blank=True, null=True)
+
 
     def save(self, *args, **kwargs):
         self.prestamo.saldo_pendiente -= self.monto_pagado
